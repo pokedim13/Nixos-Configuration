b115e7d1943c9ca1df886158891c38579fa6b5e5
sweetdogs <skinxedovich@vk.com>
2025-04-14 22:28:31 +0000

    add btrfs module

---
+++ config/hosts/x86_64-linux/common/modules/btrfs/default.nix
@@ -0,0 +1,55 @@
+{ lib, ... }: {
+  disko.devices = lib.mkDefault {
+    disk = {
+      nixos = {
+        device = lib.mkDefault "/dev/sda";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            # Backward compatible with bios booting. 
+            # It also makes it easier to install grub
+            boot = {
+              name = "NIXBOOT";
+              size = "1M";
+              type = "EF02";
+            };
+
+            ESP = {
+              name = "NIXESP";
+              type = "EF00";
+              size = "128M";
+              content = {
+               type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+
+            swap = lib.mkDefault {
+              name = "NIXSWAP";
+              size = "8G";
+              content = {
+                type = "swap";
+                resumeDevice = true;
+              };
+            };
+
+            root = {
+              name = "NIXROOT";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "btrfs";
+                mountpoint = "/";
+                options = [ "compress=zstd:7" "noatime" ];
+              };
+            };
+            # Nixos disk partition END
+          };
+        };
+      };
+    };
+    # New disks
+  };
+}
